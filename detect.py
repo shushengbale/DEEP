9fd6ac6b3ef4d9794411db0b66bdf7994cb3b143
shushengbale <97353055+shushengbale@users.noreply.github.com>
2022-05-03 20:07:05 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -154,7 +154,7 @@ def detect(opt, save_img=False):
                         vid_writer = cv2.VideoWriter(
                             # save_path, cv2.VideoWriter_fourcc(*'avc3'), fps, (w, h))
                             # 参考 https://xugaoxiang.com/2021/08/20/opencv-h264-videowrite
-                            save_path, cv2.VideoWriter_fourcc(*'avc1'), fps, (w, h))
+                            save_path, cv2.VideoWriter_fourcc(*'avc3'), fps, (w, h))
                     vid_writer.write(im0)
 
     if save_txt or save_img:
